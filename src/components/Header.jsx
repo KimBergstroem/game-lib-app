ed31a39e09029fedc58031ba9b218a26954c0be9
KimBergstroem <kimmen_b@hotmail.com>
2024-11-17 17:13:01 +0100

    style(add): animation to my header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="flex items-center justify-center py-12">
-      <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-blue-500 to-indigo-600 tracking-tight hover:scale-105 transition-transform duration-300 cursor-pointer">
+      <h1 className="animate-float text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-blue-500 to-indigo-600 tracking-tight hover:scale-105 transition-transform duration-300 cursor-pointer">
         Gaming Library
         <span className="block text-lg text-blue-400 text-center mt-2">
           for n 0 0 bs

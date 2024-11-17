f2c1f27c545aff9ead42052866daa68000b6cd1d
KimBergstroem <kimmen_b@hotmail.com>
2024-11-17 15:07:24 +0100

    update: warnings in console about router v7, added startTransition and relativeSplatPath

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import Home from "@/pages/Home";
+import Layout from "@/pages/Layout";
+import GameDetail from "@/components/GameDetail";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,10 +15,15 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <BrowserRouter>
+    // For React Router v7 warnings in console
+    // https://reactrouter.com/en/6.28.0/upgrading/future#v7_starttransition
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <QueryClientProvider client={queryClient}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Layout />} />
+          <Route path="/game/:id" element={<GameDetail />} />
         </Routes>
       </QueryClientProvider>
     </BrowserRouter>

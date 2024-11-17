4dddeba151b78a13a195d29a3e39acd65f1fd783
KimBergstroem <kimmen_b@hotmail.com>
2024-11-17 13:30:46 +0100

    add: route structure with home page path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import Home from "@/pages/Home";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,9 +16,9 @@ function App() {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <h1 className="text-3xl font-bold underline">
-          Batman commit! Welcome everyone!
-        </h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </QueryClientProvider>
     </BrowserRouter>
   );
